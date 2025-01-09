670acfd245dc9ad1a03664b68697fe765355cb7e
Christopher Ramos DEV <127137042+ChrisRamosC@users.noreply.github.com>
2025-01-09 15:06:08 -0500

    First Commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#E6D8CC',
+          600: '#996830',
+        },
+        neutral: {
+          100: '#E5E5E5',
+          300: '#D9D9D6',
+          700: '#444444',
+          900: '#2C2C2C',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
